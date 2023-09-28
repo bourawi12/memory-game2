06e0b620ca6fed4bcdd5f2ee3223513d3aab187c
bourawi12 <mohamed12bouaziz@gmail.com>
2023-09-28 10:17:45 +0100

    adding id for each card

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const cards = document.querySelectorAll('.memory-card');
+function handleEvent(event) {
+  let id = event.target.id;
+  // Rest of your code using the id
+  console.log(id);
+}
 
+// Attach an event listener and pass the event object to the handler
+this.addEventListener('click', handleEvent);
 function flipCard() {
   console.log('i was clicked!');
   console.log(this);
@@ -8,4 +15,5 @@ function flipCard() {
 
 cards.forEach((card) => {
   card.addEventListener('click',flipCard);
+  //console.log(id);
 });
